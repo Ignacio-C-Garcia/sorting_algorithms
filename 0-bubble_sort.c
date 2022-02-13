a8e7787185e6877b8ed7d0f4685d5154d91a31ea
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2022-02-13 12:21:44 -0800

    betty

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,19 +6,19 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-size_t index = 0;
-int aux = 0;
+	size_t index = 0;
+	int aux = 0;
 
 
-for (index = 0; index + 1 < size; index++)
-{
-if (array[index] > array[index + 1])
-{
-aux = array[index];
-array[index] = array[index + 1];
-array[index + 1] = aux;
-print_array(array, size);
-bubble_sort(array, size);
-}
-}
+	for (index = 0; index + 1 < size; index++)
+	{
+		if (array[index] > array[index + 1])
+		{
+			aux = array[index];
+			array[index] = array[index + 1];
+			array[index + 1] = aux;
+			print_array(array, size);
+			bubble_sort(array, size);
+		}
+	}
 }
