638dbb91a6168be7944c4340cc0ab1c56cad95be
ignacio <ignacioacgarcia@gmail.com>
2022-02-14 19:13:36 -0300

    fix errors

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -6,20 +6,24 @@
  */
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, min_idx, tmp;
-  
+    size_t i = 0, j = 0, min_idx = 0, tmp = 0;
+
+    if (!array || size < 2)
+        return;
 
     for (i = 0; i < size - 1; i++)
     {
 
         min_idx = i;
-        for (j = i+1; j < size; j++)
+        for (j = i + 1; j < size; j++)
           if (array[j] < array[min_idx])
             min_idx = j;
-
-        tmp = array[min_idx];
-        array[min_idx] = array[i];
-        array[i] = tmp;
-        print_array(array, size);
+        if (array[min_idx] != array[i])
+        {
+            tmp = array[min_idx];
+            array[min_idx] = array[i];
+            array[i] = tmp;
+            print_array(array, size);
+        }   
     }
 }
