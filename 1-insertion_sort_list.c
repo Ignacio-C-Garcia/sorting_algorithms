e52df474a488969501a9e28f2fb5628d674b9f15
ignacio <ignacioacgarcia@gmail.com>
2022-02-14 12:34:42 -0300

    insertion sort

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,45 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - sort a dll
+ * @list: header of dll 
+ */
+void insertion_sort_list(listint_t **list)
+{
+
+    listint_t *head = (*list)->next;
+    listint_t *move = head->prev;
+    listint_t *aux = NULL;
+
+    while(move)
+    {
+        if (head == NULL)
+            move = move->next;
+        else
+            move = head->prev;
+
+        while (move && move->prev && move->n < move->prev->n)
+        {
+            if (move->next)
+                move->next->prev = move->prev;
+            if (move->prev->prev)
+                move->prev->prev->next = move;
+
+                aux = move->next;
+
+                move->next = move->prev;
+                move->prev->next = aux;
+
+                aux = move->prev->prev;
+                move->prev->prev = move;
+                move->prev = aux;
+
+                if ((*list)->prev)
+                    *list = (*list)->prev;
+                print_list(*list);
+        }
+        if (head)
+            head = head->next;
+    }
+
+    
+}
