7ba144c213205c859a43747234f0d6a6fdf19c5c
ignacio <ignacioacgarcia@gmail.com>
2022-02-14 12:55:00 -0300

    betty

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -5,48 +5,50 @@
  */
 void insertion_sort_list(listint_t **list)
 {
+if (!list || !(*list))
+return;
 
-    listint_t *head = NULL;
-    listint_t *move = NULL;
-    listint_t *aux = NULL;
+listint_t *head = NULL;
+listint_t *move = NULL;
+listint_t *aux = NULL;
 
-    if ((*list)->next)
-    {
-        head = (*list)->next;
-        move = head->prev;
-    }
+if ((*list)->next)
+{
+head = (*list)->next;
+move = head->prev;
+}
 
-    while(move)
-    {
-        if (head == NULL)
-            move = move->next;
-        else
-            move = head->prev;
+while(move)
+{
+if (head == NULL)
+move = move->next;
+else
+move = head->prev;
 
-        while (move && move->prev && move->n < move->prev->n)
-        {
-            if (move->next)
-                move->next->prev = move->prev;
+while (move && move->prev && move->n < move->prev->n)
+{
+if (move->next)
+move->next->prev = move->prev;
 
-            if (move->prev->prev)
-                move->prev->prev->next = move;
+if (move->prev->prev)
+move->prev->prev->next = move;
 
-            aux = move->next;
+aux = move->next;
 
-            move->next = move->prev;
-            move->prev->next = aux;
+move->next = move->prev;
+move->prev->next = aux;
 
-            aux = move->prev->prev;
-            move->prev->prev = move;
-            move->prev = aux;
+aux = move->prev->prev;
+move->prev->prev = move;
+move->prev = aux;
 
-            if ((*list)->prev)
-                *list = (*list)->prev;
-            print_list(*list);
-        }
-        if (head)
-            head = head->next;
-    }
+if ((*list)->prev)
+*list = (*list)->prev;
+print_list(*list);
+}
+if (head)
+head = head->next;
+}
 
-    
+
 }
